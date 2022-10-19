a8c5b6cdda97d2e0ca88167ecb7a2cd46c4356b8
hass <hassaan3@ualberta.ca>
2022-10-18 20:01:37 -0600

    prinot statement to verify connection

--- server/app.py
+++ server/app.py
@@ -9,6 +9,7 @@ app = FastAPI()
 def startup_db_client():
     app.mongodb_client = MongoClient("mongodb://localhost:27017")
     app.database = app.mongodb_client["socialnetwork"]
+    print("Connected to MongoDB")
 
 @app.on_event("shutdown")
 def shutdown_db_client():
