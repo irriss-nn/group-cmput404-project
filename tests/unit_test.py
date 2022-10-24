8fa331f292405d5ef8b7e95ad66f3261598215c3
Yutong Liu <yutong13@ualberta.ca>
2022-10-24 14:19:02 -0600

    Fix unit tests, put tests in one file

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,11 +1,95 @@
 from urllib import response
 from fastapi.testclient import TestClient
-import sys
-sys.path.append('../server')
-from server.app import app, startup_db_client, shutdown_db_client
+from pathlib import Path
+import os
+
+from requests import request
+
+from models import author
+os.chdir(Path.cwd().parent)
+from main import app, startup_db_client, shutdown_db_client
 
 client = TestClient(app)
 
+def test_read_main():
+    '''An example unit test'''
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World"}
+
+Fake_Author = {
+    "id": "fakeid1",
+    "url":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+    "host":"http://127.0.0.1:8000/",
+    "displayName":"Fake Croft",
+    "github": "http://github.com/laracroft",
+    "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+}
+
+def test_add_author():
+    '''Add author via post request'''
+    # TODO: probably remove this test in future when starts using data import to database directly 
+    startup_db_client()
+
+    response = client.post("/service/authors/",headers={"Content-Type":"application/json"}, json = Fake_Author)
+    author = response.json()
+    assert response.status_code == 200
+    assert author["_id"] == Fake_Author["id"]
+
+    shutdown_db_client()
+
+def test_get_one_author():
+    '''Get added author'''
+    startup_db_client()
+    response = client.get(f"/service/authors/{Fake_Author['id']}",headers={"Content-Type":"application/json"}, json = Fake_Author)
+    author = response.json()
+    assert response.status_code==200
+    assert author["author_id"] == Fake_Author["id"]
+
+def test_get_authors():
+    startup_db_client()
+    '''Use get method to get all authors in one time'''
+    Fake_Author2 = {
+                    "id": "fakeid2",
+                    "url":"http://127.0.0.1:5454/authors/fakeauthor2",
+                    "host":"http://127.0.0.1:8000/",
+                    "displayName":"Fake Thompson",
+                    "github": "http://github.com/fakeThopson",
+                    "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+                }
+    client.post("/service/authors/",headers={"Content-Type":"application/json"}, json = Fake_Author2)
+    
+    response = client.get(f"/service/authors/",headers={"Content-Type":"application/json"})
+    authors = response.json()
+    for i in range(len(authors)):
+        authors[i] = authors[i]["_id"]
+    assert response.status_code == 200
+    assert Fake_Author["id"] in authors
+    assert Fake_Author2["id"] in authors
+    shutdown_db_client()
+
+    
+
+def test_update_author():
+    startup_db_client()
+    '''Update an author's attribute via post method'''
+    Fake_Author_Modified = {
+                    "id": "fakeid1",
+                    "url":"http://127.0.0.1:5454/authors/9de17f29c12e8f97bcbbd34cc908f1baba40658e",
+                    "host":"http://127.0.0.1:8000/",
+                    "displayName":"Modified Croft",
+                    "github": "http://github.com/modified",
+                    "profileImage": "https://i.imgur.com/k7XVwpB.jpeg"
+                }
+    response = client.post("/service/authors/",headers={"Content-Type":"application/json"}, json = Fake_Author_Modified)
+    author = response.json()
+    assert response.status_code == 200
+    assert author["displayName"]== Fake_Author_Modified["displayName"]
+
+    # remove fake authors
+    app.database["authors"].delete_one({"_id":"fakeid1"})
+    app.database["authors"].delete_one({"_id":"fakeid2"})
+    shutdown_db_client()
 
 Fake_Post = {
             "type":"post",
