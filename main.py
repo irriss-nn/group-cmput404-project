6a814c11ba6d4d2e9356c00ebcc73731ec5b8db3
Dayyan Muhammad <m.dayyan367@gmail.com>
2022-10-24 22:10:09 -0600

    post completed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import uvicorn
-
 from fastapi import FastAPI, APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -22,35 +21,48 @@ app.mount("/static", StaticFiles(directory=static_dir), name="static")
 app.include_router(authors.router)
 app.include_router(posts.router)
 
+
 @app.on_event("startup")
 def startup_db_client():
     app.mongodb_client = MongoClient("mongodb://localhost:27017")
     app.database = app.mongodb_client["socialnetwork"]
     print("Connected to MongoDB")
 
+
 @app.on_event("shutdown")
 def shutdown_db_client():
     app.mongodb_client.close()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/login", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
+
 
 @app.get("/register", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
 
+
 @app.post("/register")
 async def register_author(request: Request):
     return {"message": "register"}
 
+
 @app.post("/login")
 async def read_item(request: Request):
     return {"message": "login"}
+
+
+@app.get("/post", response_class=HTMLResponse)
+async def get_post(request: Request):
+    foundPosts = request.app.database["post"].find({})
+    return templates.TemplateResponse("post.html", {"request": request, "post": foundPosts[5]})
 
 
 if __name__ == "__main__":
