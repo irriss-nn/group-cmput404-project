cf99b2d7ec9d1280c431518ea3b0773accad6909
hass <hassaan3@ualberta.ca>
2022-10-25 23:13:06 -0600

    error handling for comments

--- routers/comments_router.py
+++ routers/comments_router.py
@@ -10,16 +10,28 @@ router = APIRouter(
 
 @router.post("/{author_id}/posts/{post_id}/comments")
 async def create_comment(author_id: str,post_id:str, request: Request, comment: Comment = Body(...)):
-    comment = jsonable_encoder(comment)
-    comment['author']=author_id
-    comment['post']=post_id
-    comment["_id"] = comment["id"]
-    comment.pop('id', None)
-    comment_id = comment["_id"]
-    # inserting comment into database
-    request.app.database["comments"].insert_one(comment)
-    # Adding comment to post
-    return request.app.database["comments"].find_one({"_id": comment_id})
+    # See if we can find the post and author to validate them first
+
+    try:
+        post = request.app.database["post"].find_one({"_id":post_id})
+        author = request.app.database["authors"].find_one({"_id":author_id})
+        if(author == None or post == None):
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Author or post not found")
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post or author not found")
+    try:
+        comment = jsonable_encoder(comment)
+        comment['author']=author_id
+        comment['post']=post_id
+        comment["_id"] = comment["id"]
+        comment.pop('id', None)
+        comment_id = comment["_id"]
+        # inserting comment into database
+        request.app.database["comments"].insert_one(comment)
+        # Adding comment to post
+        return request.app.database["comments"].find_one({"_id": comment_id})
+    except:
+        raise HTTPException(status_code=404, detail="Invalid Comment")
 
 @router.get("/{author_id}/posts/{post_id}/comments")
 async def read_comments(request: Request, author_id:str, post_id:str, page: int|None = None, size: int|None = None):
