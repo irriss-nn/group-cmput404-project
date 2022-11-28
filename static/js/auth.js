61cb5dadcde37778c296b7eb3093c34b39b08f3e
Michael Huang <michael@mayoduckpie.com>
2022-11-28 15:13:44 -0700

    Implement basic credential handler

---
+++ static/js/auth.js
@@ -0,0 +1,7 @@
+// TODO: Implement node auth
+function getCredentials(host) {
+    if (host === 'https://socioecon.herokuapp.com')
+        return `${btoa('team7:pot8os_are_yummy')}`
+
+    return ''
+}
