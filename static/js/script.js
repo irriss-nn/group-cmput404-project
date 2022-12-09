9c8aa04ff8e0e9bc74f7f5341624d11cc97a42cd
Dayyan Muhammad <m.dayyan367@gmail.com>
2022-12-09 00:05:22 -0700

    sharing setup

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,45 @@
 const search = document.querySelector("#search-form");
+const shareBtn = document.querySelector("#sharebtn");
+
+const sharePopup = document.querySelector("#share-model");
+const shareClose = document.querySelector(".popup-close");
+
+const following = document.querySelectorAll(".person-share-btn");
+
+let shareUrl;
 
 search.addEventListener("submit", (e) => {
   e.preventDefault();
-  let name = (e.target[0].value).replace(/ /g,"_");
+  let name = e.target[0].value.replace(/ /g, "_");
   window.location.replace(`/author/${name}`);
+});
+
+shareBtn.addEventListener("click", (e) => {
+  shareUrl = e.target.getAttribute("data-url");
+  sharePopup.show();
+});
+
+shareClose.addEventListener("click", () => {
+  console.log("close");
+  sharePopup.close();
+});
+
+following.forEach((person) => {
+  person.addEventListener("click", (e) => {
+    console.log(e.target.getAttribute("data-author"));
+    shareUrl += e.target.getAttribute("data-author");
+
+    // fetch(shareUrl, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  });
 });
